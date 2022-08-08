b8c5da1884758ed2f4043e3fa066809a67faf117
yank1 <yancarloglez98@gmail.com>
2022-08-08 11:53:52 -0400

    to deploy

--- src/red/endpoints.js
+++
@@ -1,6 +0,0 @@
-//const base_url = 'http://localhost:3001/'
-const prod_base_url = "https://immense-badlands-98923.herokuapp.com/"
-const base_url = 'http://192.168.236.198:3001/'
-export default {
-    base_url: prod_base_url
-}
